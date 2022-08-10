c6f63eb0a8d6a1b50f8364daf091c479d4e48d69
nassetti <nassettimax@gmail.com>
2022-08-10 22:28:52 +0000

    Create model

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
 # Gratia
 
-Gratia is a daily journalling application, centred around the Stoic concept
+Gratia is a daily journal application, centred around that which is often taken for granted - things we are grateful for.
 
 ## Agile Methods
+
 
 ## Features
 
 ## Data
+
+### Post Method
+| Key          | Name           | Target     |
+|--------------|----------------|------------|
+| ForeignKey   | Author         | User Model |
+|              | Created Date   | DateTime   |
+|              | Updated Date   | DateTime   |
+|              | List Item 1    | TextField  |
+|              | List Item 2    | TextField  |
+|              | List Item 3    | TextField  |
+|              | List Item 4    | TextField  |
+|              | List Item 5    | TextField  |
+| Many to Many | Likes          | User Model |
+|              | Slug (Unique)  | SlugField  |
+|              | Title (Unique) | Char(200)  |
+|              | Status         | Integer    |
 
 ## Testing
 
--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,38 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+STATUS = ((0, "Private"), (1, "Public"))
+
+
+class Journal(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name="journal_entry")
+    updated_on = models.DateTimeField(auto_now=True)
+    item_one = models.TextField()
+    item_two = models.TextField()
+    item_three = models.TextField()
+    item_four = models.TextField()
+    item_five = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='journal_likes',
+        blank=True)
+
+
+class Meta:
+    ordering = ['-created_on']
+
+
+def __str__(self):
+    return self.title
+
+
+def number_of_likes(self):
+    return self.likes.count()
+
