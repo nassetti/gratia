49639f29bcbe09da9e3ca3307a146426d48cc430
nassetti <nassettimax@gmail.com>
2022-08-23 15:14:19 +0000

    update views

--- journal/views.py
+++ journal/views.py
@@ -8,7 +8,3 @@ class JournalList(generic.ListView):
     model = Journal 
     queryset = Journal.objects.filter(status=1).order_by('-created_on')
     paginate_by = 25
-
-
-def taskList(request):
-    return HttpResponse('Gratia Application')
